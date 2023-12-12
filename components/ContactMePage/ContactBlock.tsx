45389a26446a2dfdb828700316dc0937f5f02da7
madeline tennier <madeline.tennier@gmail.com>
2023-12-11 18:59:51 -0800

    update contact me page update secondary text color

--- components/ContactMePage/ContactBlock.tsx
+++ components/ContactMePage/ContactBlock.tsx
@@ -3,29 +3,35 @@ import {Divider} from 'antd'
 const ContactBlock = () => {
     return (
         <div className = {styles.contactContainer}>
-            <div className={styles.contactBlock}>
-                
-                <h1 className={styles.headerText}>
-                    Contact Me
-                </h1>
-                <div className={styles.divider}>
-                    <Divider type='vertical' style={{height:500}}/>
+            <div className={styles.contactHeaderBlock}>
+                <div className= {styles.contactHeaderText}>
+                    <h1>
+                        Contact Me
+                    </h1>
                 </div>
-                <div className={styles.contactTextBlock}>
-                    <div className={styles.contactText}>
-                    <p>
-                        thank you for wanting to get in contact with me!
-                    </p>
-                    <br />
-                    <p>
-                        my linkedin can be found <a target='_blank' href='//www.linkedin.com/in/madeline-tennier-067392233' rel='noreferrer'>here.</a>
-                    </p>
-                    <br />
-                    <p>
-                        you can also contact me directly at madeline.tennier@gmail.com.
-                    </p>
-                    <br />
-                    </div>
+            </div>
+            <div className={styles.contactInfoBlock}>
+                <div className = {styles.contactInfoText}>
+                     <p>
+                        Thank you for wanting to get in contact with me!
+                     </p>
+                     <br />
+                     <p>
+                        My LinkedIn can be found <a target='_blank' href='//www.linkedin.com/in/madeline-tennier-067392233' rel='noreferrer'>here</a>.
+                     </p>
+                     <br />
+                     <p>
+                        You can also contact me directly at madeline.tennier@gmail.com.
+                     </p>
+                     <br />
+                     <p>
+                        If youre looking for my resume, you can find it in the header above - 
+                        my github is also there!
+                     </p>
+                     <br />
+                     <p>
+                        
+                     </p>
                 </div>
             </div>
         </div>

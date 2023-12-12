9006eca61e3619add3d3746e1e37f9ef0c0034f6
Madeline Tennier <88593336+mtennier@users.noreply.github.com>
2023-12-11 19:21:44 -0800

    link changes (#6)
    fixing colors on links & adding links to header

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -1,17 +1,24 @@
 import styles from './Header.module.scss'
+import Link from 'next/link'
 
 const Header = () => {
 return (<>
 <div className={styles.headerContainer}>
     <div className={styles.headerText}>
-        Madeline Tennier
+        <Link href='/'className={styles.headerLink}>
+            Madeline Tennier
+        </Link>
     </div>
     <div className={styles.subHeaderText}>
         <span className={styles.subHeaderItem}>
+            <a target='_blank' href='//www.github.com/mtennier' rel='noreferrer' className={styles.subHeaderLink}>
             github
+            </a>
         </span>
         <span className={styles.subHeaderItem}>
+            <Link href='/contact-me' className={styles.subHeaderLink}>
             contact
+            </Link>
         </span>
         <span className={styles.subHeaderItem}>
             resume

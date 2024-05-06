b55f0d8ac10589f7b7b875c5fb9609eba54d103a
madeline tennier <madeline.tennier@gmail.com>
2024-05-06 10:39:52 -0700

    commenting out knitting page temp while figuring out deploy

--- src/pages/knitting.tsx
+++ src/pages/knitting.tsx
@@ -1,31 +1,31 @@
-import { promises as fs } from "fs";
-import path from "path";
-import PhotoGallery from "../../components/PhotoGallery/PhotoGallery";
+// import { promises as fs } from "fs";
+// import path from "path";
+// import PhotoGallery from "../../components/PhotoGallery/PhotoGallery";
 
 
 
-export default function Knitting({ photos }: {photos:any}) {
-  const images = JSON.parse(photos);
-  return (
-    <>
-      <p>knitting</p>
-      <PhotoGallery images={images} />
-    </>
-  );
-}
+// export default function Knitting({ photos }: {photos:any}) {
+//   const images = JSON.parse(photos);
+//   return (
+//     <>
+//       <p>knitting</p>
+//       <PhotoGallery images={images} />
+//     </>
+//   );
+// }
 
-// Function to retrieve photos.
-export async function getStaticProps() {
-  // Retrieve photos
-  const getPhotos = async () => {
-    const imageDirectory = path.join(process.cwd(), "/public/knitting-images");
-    const imageFilenames = await fs.readdir(imageDirectory);
-    // add path to all filenames
-    const imagePaths = imageFilenames.map((img) => {
-      return "/knitting-images/" + img;
-    });
-    return JSON.stringify(imagePaths);
-  };
-  const images = await getPhotos();
-  return { props: { photos: images } };
-}
+// // Function to retrieve photos.
+// export async function getStaticProps() {
+//   // Retrieve photos
+//   const getPhotos = async () => {
+//     const imageDirectory = path.join(process.cwd(), "/public/knitting-images");
+//     const imageFilenames = await fs.readdir(imageDirectory);
+//     // add path to all filenames
+//     const imagePaths = imageFilenames.map((img) => {
+//       return "/knitting-images/" + img;
+//     });
+//     return JSON.stringify(imagePaths);
+//   };
+//   const images = await getPhotos();
+//   return { props: { photos: images } };
+// }
